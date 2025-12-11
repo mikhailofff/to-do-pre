4d52b40550aa27b599f4728bbb68af0b91127169
mikhailofff <asdgrghd@gmail.com>
2025-12-11 16:14:14 +0400

    feat: displayed current tasks

--- index.js
+++ index.js
@@ -12,17 +12,20 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-
+	return items;
 }
 
 function createItem(item) {
 	const template = document.getElementById("to-do__item-template");
 	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-  const textElement = clone.querySelector(".to-do__item-text");
-  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-  const editButton = clone.querySelector(".to-do__item-button_type_edit");
+	const textElement = clone.querySelector(".to-do__item-text");
+	const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+	const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+	const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
+	textElement.textContent = item;
+
+	return clone;
 }
 
 function getTasksFromDOM() {
@@ -33,3 +36,8 @@ function saveTasks(tasks) {
 
 }
 
+items = loadTasks();
+
+items.forEach(function (item) {
+	listElement.append(createItem(item));
+})
